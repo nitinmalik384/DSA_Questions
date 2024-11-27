c1f1c90110d4b589d6f5005f520534f4c3e74b8c
nitinmalik384 <nitinmalik384@gmail.com>
2024-11-27 12:45:28 +0530

    added Union of two sorted arrays solution

---
+++ Arrays/Union.cpp
@@ -0,0 +1,78 @@
+// // Union of two Sorted Arrays
+// // Question Link : https://bit.ly/42SxAfe
+
+// Problem statement
+// Given two sorted arrays, ‘a’ and ‘b’, of size ‘n’ and ‘m’, respectively, return the union of the arrays.
+
+
+
+// The union of two sorted arrays can be defined as an array consisting of the common and the distinct elements of the two arrays. The final array should be sorted in ascending order.
+
+
+
+// Note: 'a' and 'b' may contain duplicate elements, but the union array must contain unique elements.
+
+
+
+// Example:
+// Input: ‘n’ = 5 ‘m’ = 3
+// ‘a’ = [1, 2, 3, 4, 6]
+// ‘b’ = [2, 3, 5]
+
+// Output: [1, 2, 3, 4, 5, 6]
+
+// Explanation: Common elements in ‘a’ and ‘b’ are: [2, 3]
+// Distinct elements in ‘a’ are: [1, 4, 6]
+// Distinct elements in ‘b’ are: [5]
+// Union of ‘a’ and ‘b’ is: [1, 2, 3, 4, 5, 6]
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+
+int main()
+
+{
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+  vector < int > Union = FindUnion(arr1, arr2, n, m);
+  cout << "Union of arr1 and arr2 is  " << endl;
+  for (auto & val: Union)
+    cout << val << " ";
+  return 0;
+}
+
