a1419ac2dab58e3ea3a3c4b90d4cfe8ecfb4344a
nitinmalik384 <nitinmalik384@gmail.com>
2024-12-17 12:52:55 +0530

    Question: Binary Search

---
+++ Binary Search/BinarySearch.cpp
@@ -0,0 +1,49 @@
+// Binary Search
+// Problem Link: https://leetcode.com/problems/binary-search/description/
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int search(vector<int> &nums, int target)
+{
+    int found = -1;
+    int low = 0, high = nums.size() - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] > target)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return found;
+}
+int main()
+{
+    // Write C++ code here
+    vector<int> prices = {1, 1, 5, 7, 8, 77};
+    cout << search(prices, 7) << endl;
+
+    return 0;
+}
